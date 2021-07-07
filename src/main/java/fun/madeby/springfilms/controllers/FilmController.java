978c65bce018e23c69405c83d9a9e6d89a9af428
Graham Duthie <grahamd483@gmail.com>
2021-07-07 11:54:13 +0200

    Add new film via form working, without advantage of Spring, ie. all parameters set manually.

--- src/main/java/fun/madeby/springfilms/controllers/FilmController.java
+++ src/main/java/fun/madeby/springfilms/controllers/FilmController.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @Controller
@@ -17,13 +20,27 @@ public class FilmController {
 	private final FilmService filmService;
 
 // Route Only
-@GetMapping(value="/films/newFilmForm")
+@GetMapping(value="/films/new")
 public String newFilmForm() {
 	return "newFilmForm";
 }
 
+// POST how you'd expect to do it.
+@PostMapping(value="/films/new")
+public String postFilmRequested(Model model,
+                                @RequestParam String title,
+                                @RequestParam("releaseYear") String releaseYear,
+                                @RequestParam("filmImageUrl") String filmImageUrl) {
+	Film newFilm = new Film();
+	newFilm.setTitle(title);
+	int year = Integer.parseInt(releaseYear);
+	newFilm.setReleaseDate(LocalDate.of(year, Month.JANUARY, 1));
+	newFilm.setFilmImageUrl(filmImageUrl);
+	filmService.register(newFilm);
+	return "newFilmForm";
+}
 
-// Route & Request
+// Route & GET
 @GetMapping("/films")
 public String allFilmsRequested(Model model) {
 	List<Film> allMyFilms = filmService.retrieveAll();
@@ -49,6 +66,7 @@ public String searchFilmRequested(Model model,
 	model.addAttribute("kumquat", searchTermFilms);
 	return "films";
 }
+
 
 
 }
