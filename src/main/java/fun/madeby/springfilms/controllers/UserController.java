3033d0e07db2a459f02a16e82de50ffac1d920a4
Graham Duthie <grahamd483@gmail.com>
2021-07-09 10:31:36 +0200

    All added researched and written up, but not tested

--- src/main/java/fun/madeby/springfilms/controllers/UserController.java
+++ src/main/java/fun/madeby/springfilms/controllers/UserController.java
@@ -1,13 +1,22 @@
 package fun.madeby.springfilms.controllers;
 
+import fun.madeby.springfilms.models.User;
+import fun.madeby.springfilms.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+	private final UserService userService;
 
 	@GetMapping("login")
 	public String login() {
@@ -16,5 +25,28 @@ public class UserController {
 			return "login";
 		return "redirect:/";
 	}
+
+	@GetMapping("register")
+	public String registerForm(Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null) // eventually the button wont even be shown and this will be redundant
+			return "";
+		model.addAttribute("inkBlot", new User());
+		return "register";
+	}
+
+	@PostMapping("/register")
+	public String registerPost(User inkBlot, BindingResult bindingResult) {
+
+		if(bindingResult.hasErrors())
+			return"register";
+
+		userService.register(inkBlot);
+		return "redirect:/login";
+	}
+
+
+
+
 
 }
---
+++ src/main/resources/static/js/passwordMatch.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#txtNewPassword, #txtConfirmPassword").keyup(checkPasswordMatch);
+});
+
+function checkPasswordMatch() {
+    var password = $("#txtNewPassword").val();
+    var confirmPassword = $("#txtConfirmPassword").val();
+
+    if (password != confirmPassword)
+        $("#divCheckPasswordMatch").html("Passwords do not match!");
+    else
+        $("#divCheckPasswordMatch").html("Passwords match.");
+}
+
