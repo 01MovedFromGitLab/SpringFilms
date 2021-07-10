488517760280cb80e21417e2d30718207b18a802
Graham Duthie <grahamd483@gmail.com>
2021-07-10 13:34:39 +0200

    Was working, remade db because of password issue, not 403 to edit profile when logged in ???

--- src/main/java/fun/madeby/springfilms/controllers/ProfileController.java
+++ src/main/java/fun/madeby/springfilms/controllers/ProfileController.java
@@ -8,7 +8,9 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
@@ -39,6 +41,14 @@ public String myProfileEditView(Model model, Principal principal) {
 	User userProfile = userService.retrieveByUsername(principal.getName());
 	model.addAttribute("profile", userProfile );
 	return "myProfileEdit";
+}
+
+@PostMapping("my-profile/edit")
+public String postEditedProfile(User profile, BindingResult br) {
+	if(br.hasErrors())
+		return "my-profile/edit";
+	userService.register(profile);
+	return "redirect:/my-profile?updated";
 }
 
 }
